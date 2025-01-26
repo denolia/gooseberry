fc50aecb9a0bf1b0234cc2a4aac1ef432e3bafb4
Julia Bubnova <bubnova.j.i@gmail.com>
2025-01-26 16:53:25 +0100

    Improve prompt again

--- src/app/api/translate/route.ts
+++ src/app/api/translate/route.ts
@@ -18,7 +18,7 @@ export async function POST(request: Request) {
           {
             role: "system",
             content: `You are a translator. Translate German text to Russian.
-             If the input is a sentence, provide only translation without any additional comments. If the input is a one word or a phrase, then provide all possible translations for it, show where to put stress in a word, show how to use the word in 5 sentences in German with translations in Russian. When possible make the sentences illustrate different ways of using a word. If it is a noun, then add a definite article (der/die/das), show singular, plural forms and Genitiv case form. If the input is a verb, then write 3rd person form, Präteritum and Perfect forms with haben or sein. If the verb has a stable connection with prepositions and cases, writ it as well.`,
+             If the input is a sentence, provide only translation without any additional comments. If the input is a one word or a phrase, then provide all possible translations for it.  Write IPA  transcription. If it is a noun, then add a definitive article (der/die/das), show singular, plural and Genitiv case forms. If the input is a verb, then write Presens 3rd person, Präteritum and Perfect forms with haben or sein. If the verb has a fixed Verb-Noun or Verb-Preposition-Case, write it as well. Then show how to use the word in 5 sentences in German with translations in Russian. When possible make the sentences illustrate different ways of using the word. `,
           },
           {
             role: "user",
