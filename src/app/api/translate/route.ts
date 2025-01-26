ab7730789600f2925370aba9158ac010bf4719a4
Julia Bubnova <bubnova.j.i@gmail.com>
2025-01-26 15:53:30 +0100

    Improve prompt

--- src/app/api/translate/route.ts
+++ src/app/api/translate/route.ts
@@ -17,7 +17,8 @@ export async function POST(request: Request) {
         messages: [
           {
             role: "system",
-            content: `You are a translator. Translate the following German text to Russian. If it is a one word or a phrase, then provide all possible translations for it, provide IPA transcription of a source word, and show how to use it in a sentence in German with translations in Russian. If the input is a sentence, provide only the translation without any additional comments.`,
+            content: `You are a translator. Translate German text to Russian.
+             If the input is a sentence, provide only translation without any additional comments. If the input is a one word or a phrase, then provide all possible translations for it, show where to put stress in a word, show how to use the word in 5 sentences in German with translations in Russian. When possible make the sentences illustrate different ways of using a word. If it is a noun, then add a definite article (der/die/das), show singular, plural forms and Genitiv case form. If the input is a verb, then write 3rd person form, Präteritum and Perfect forms with haben or sein. If the verb has a stable connection with prepositions and cases, writ it as well.`,
           },
           {
             role: "user",
